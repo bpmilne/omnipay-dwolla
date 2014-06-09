278f8e3dd70652bf2692e89894cb2f860896d18c
mach-kernel <ds4028@nyu.edu>
2014-06-09 14:28:45 -0400

    reflectionmethod; invoke protected members

--- tests/Omnipay/Message/Dwolla/PurchaseRequestTest.php
+++ tests/Omnipay/Message/Dwolla/PurchaseRequestTest.php
@@ -8,6 +8,9 @@ class PurchaseRequestTest extends TestCase
 {
     public function setUp()
     {
+        $getP = new ReflectionMethod('AbstractRequest', 'getParameter');
+        $getP->setAccessible(true);
+
         $this->request = new PurchaseRequest($this->getHttpClient(), $this->getHttpRequest());
         $this->request->addItem(array('name' => "NOODLES",
                                       'description' => "POODLES",
@@ -16,20 +19,20 @@ class PurchaseRequestTest extends TestCase
         $this->request->initialize(
                 array('Key'                 => 'lRlAsej0WiwbXcvXv3Y4JaMD6uEt96kDs78fZNApKBkl8De7rD',
                       'Secret'              => 'HGYW7weJfk+QC50x5TdW+cuuMEIyoYRIQE/FDr3XAAf5YnOvX2',
-                      'AllowFundingSources' => $this->request->getParameter('AllowFundingSources'),
-                      'AllowGuestCheckout'  => $this->request->getParameter('AllowGuestCheckout'),
-                      'Callback'            => $this->request->getParameter('Callback') ? 
-                       $this->request->getParameter('Callback') : null,
-                      'Redirect'            => $this->request->getParameter('Redirect') ?
-                       $this->request->getParameter('Redirect') : null,
+                      'AllowFundingSources' => $getP('AllowFundingSources'),
+                      'AllowGuestCheckout'  => $getP('AllowGuestCheckout'),
+                      'Callback'            => $getP('Callback') ? 
+                       $getP('Callback') : null,
+                      'Redirect'            => $getP('Redirect') ?
+                       $getP('Redirect') : null,
                       'PurchaseOrder'       => array(
                         'DestinationId' => '812-111-7219',
-                        'orderItems'    => $this->request->getParameter('gatewaySession'),
+                        'orderItems'    => $getP('gatewaySession'),
                         'discount'      => $discount,
                         'shipping'      => $shipping,
                         'tax'           => $tax,
                         'total'         => round($subtotal - $discount + $shipping + $tax, 2),
-                        'notes'         => $this->request->getParameter('notes') ? $this->request->getParameter('notes') : null
+                        'notes'         => $getP('notes') ? $getP('notes') : null
                       )
         ));
     }
