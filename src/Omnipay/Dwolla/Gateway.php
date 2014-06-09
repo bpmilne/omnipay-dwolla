8ee420e7097a0e7fcb5107244e3e86ac96af293e
mach-kernel <ds4028@nyu.edu>
2014-06-09 13:36:36 -0400

    oops

---
+++ src/Omnipay/Dwolla/Gateway.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Omnipay\Dwolla;
+
+use Omnipay\Common\AbstractGateway;
+use Omnipay\Dwolla\Message\PurchaseRequest;
+use Omnipay\Dwolla\Message\AbstractRequest;
+
+/**
+ * Dwolla Gateway
+ *
+ * @link https://developers.dwolla.com/dev/docs
+ */
+
+class Gateway extends AbstractGateway
+{
+    public function getName()
+    {
+        return 'Dwolla';
+    }
+
+    /*
+    View all parameters at: https://developers.dwolla.com/dev/pages/gateway#server-to-server
+    */
+    public function getDefaultParameters()
+    {
+        return array(
+            // Required
+            'key_secret' => array('key' => '',
+                                  'secret' => ''),
+            'sandbox' => false,
+            'gatewaySession' => false,
+            'purchaseOrder' => false,
+            'Callback' => false,
+            'Redirect' => false,
+            'DestinationId' => false,
+            'shipping' => false,
+            'tax' => false,
+
+            // Optional, set to defaults
+            'discount' => false,
+            'notes' => false,
+            'AllowFundingSources' => true,
+            'AllowGuestCheckout'  => true
+        );
+    }
+
+    public function getKeySecret()
+    {
+        return $this->getParameter('key_secret');
+    }
+
+    public function setKeySecret($value)
+    {
+        return $this->setParameter('key_secret', $value);
+    }
+
+    public function getSandboxMode()
+    {
+        return $this->getParameter('sandbox');
+    }
+
+    public function setSandboxMode($value)
+    {
+        return $this->setParameter('sandbox', $value);
+    }
+
+    /**
+    *   $parameters = array(
+    *      'Name' => $name,
+    *      'Description' => $description,
+    *      'Price' => $price,
+    *      'Quantity' => $quantity
+    *   );
+    */
+
+    public function addItem(array $parameters = array())
+    {
+        if (!$this->getParameter('gatewaySession')) {
+            $this->clearItems();
+        }
+        return $this->setParameter('gatewaySession', $parameters);
+    }
+    public function clearItems()
+    {
+        return $this->setParameter('gatewaySession', array());
+    }
+
+    /**
+     * @param array $parameters
+     * @return \Omnipay\Dwolla\Message\PurchaseRequest
+     */
+    public function purchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Dwolla\Message\PurchaseRequest', $parameters);
+    }
+}
