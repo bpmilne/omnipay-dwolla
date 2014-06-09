d9bda20ab3e39b86ed3a6e8d1678430eab151828
mach-kernel <ds4028@nyu.edu>
2014-06-09 14:03:44 -0400

    qualified ->request

--- src/Omnipay/Dwolla/Message/PurchaseRequest.php
+++ src/Omnipay/Dwolla/Message/PurchaseRequest.php
@@ -11,30 +11,30 @@ class PurchaseRequest extends AbstractRequest
     {
         $subtotal = 0;
 
-        foreach ($this->getParameter('gatewaySession') as $product) {
+        foreach ($this->request->getParameter('gatewaySession') as $product) {
             $subtotal += floatval($product['Price']) * floatval($product['Quantity']);
         }
 
-        $discount = $this->getParameter('discount') ? $this->getParameter('discount') : 0;
-        $shipping = $this->getParameter('shipping') ? $this->getParameter('shipping') : 0;
-        $tax = $this->getParameter('tax') ? $this->getParameter('tax') : 0;
+        $discount = $this->request->getParameter('discount') ? $this->request->getParameter('discount') : 0;
+        $shipping = $this->request->getParameter('shipping') ? $this->request->getParameter('shipping') : 0;
+        $tax = $this->request->getParameter('tax') ? $this->request->getParameter('tax') : 0;
 
         $keysec = $this->getParameter('key_secret');
 
         $data = array('Key'                 => $keysec['key'],
                       'Secret'              => $keysec['secret'],
-                      'AllowFundingSources' => $this->getParameter('AllowFundingSources'),
-                      'AllowGuestCheckout'  => $this->getParameter('AllowGuestCheckout'),
-                      'Callback'            => $this->getParameter('Callback') ? $this->getParameter('Callback') : null,
-                      'Redirect'            => $this->getParameter('Redirect') ? $this->getParameter('Redirect') : null,
+                      'AllowFundingSources' => $this->request->getParameter('AllowFundingSources'),
+                      'AllowGuestCheckout'  => $this->request->getParameter('AllowGuestCheckout'),
+                      'Callback'            => $this->request->getParameter('Callback') ? $this->request->getParameter('Callback') : null,
+                      'Redirect'            => $this->request->getParameter('Redirect') ? $this->request->getParameter('Redirect') : null,
                       'PurchaseOrder'       => array(
-                        'DestinationId' => $this->getParameter('DestinationId'),
-                        'orderItems'    => $this->getParameter('gatewaySession'),
+                        'DestinationId' => $this->request->getParameter('DestinationId'),
+                        'orderItems'    => $this->request->getParameter('gatewaySession'),
                         'discount'      => $discount,
                         'shipping'      => $shipping,
                         'tax'           => $tax,
                         'total'         => round($subtotal - $discount + $shipping + $tax, 2),
-                        'notes'         => $this->getParameter('notes') ? $this->getParameter('notes') : null
+                        'notes'         => $this->request->getParameter('notes') ? $this->request->getParameter('notes') : null
                       )
                      );
 
@@ -43,6 +43,6 @@ class PurchaseRequest extends AbstractRequest
 
     public function getEndpoint()
     {
-        return ($this->getParameter('sandbox') ? $this->sandbox_host : $this->host) . 'payment/request/';
+        return ($this->request->getParameter('sandbox') ? $this->sandbox_host : $this->host) . 'payment/request/';
     }
 }
